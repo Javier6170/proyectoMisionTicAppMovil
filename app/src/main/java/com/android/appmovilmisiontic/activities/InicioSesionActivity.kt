c9155e0785a4f7953ddeea052fe01c95d5e6bfb4
Javier Rodriguez Marulanda <rodriguez.javier.6170@eam.edu.co>
2022-10-22 07:01:42 -0500

    añadiendo las activities del inicio de dsesion y el main activitie funcional

---
+++ app/src/main/java/com/android/appmovilmisiontic/activities/InicioSesionActivity.kt
@@ -0,0 +1,12 @@
+package com.android.appmovilmisiontic.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.appmovilmisiontic.R
+
+class InicioSesionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio_sesion)
+    }
+}
