e0a06c59f4cc8b2246a8a337216639d478c27a34
JuanH941214 <juanhernandez941214@gmail.com>
2022-10-24 17:45:10 +0200

    Revert "Merge branch 'main' of https://github.com/Javier6170/proyectoMisionTicAppMovil"
    This reverts commit 2f42880b7dcee2d6a5ad898dc5dc5f0e8c71f1c4, reversing
    changes made to b78c2623824699f44d0f8cee7f745975877710c6.

--- app/src/main/java/com/android/appmovilmisiontic/MainActivity.kt
+++ app/src/main/java/com/android/appmovilmisiontic/MainActivity.kt
@@ -1,25 +1,11 @@
 package com.android.appmovilmisiontic
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.android.appmovilmisiontic.R
-import com.android.appmovilmisiontic.activities.InicioSesionActivity
-import com.android.appmovilmisiontic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.btnIngresoApp.setOnClickListener { irPantallaIngreso() }
-    }
-
-    fun irPantallaIngreso() {
-        startActivity( Intent(this, InicioSesionActivity::class.java) )
+        setContentView(R.layout.activity_main)
     }
 }
